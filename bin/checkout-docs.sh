3ebbf06e39b180a849cad4d52223765ec97de953
Tony Messias <tonysm@hey.com>
2022-09-18 12:11:57 -0300

    Adds the checkout-docs bashscript and sources and docs folders

---
+++ bin/checkout-docs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+DOCS_VERSIONS=(
+    1.x
+)
+
+for v in "${DOCS_VERSIONS[@]}"; do
+    if [ -d "resources/sources/$v" ]; then
+        echo "Pulling latest documentation updates for $v..."
+        (cd resources/sources/$v && git pull)
+    else
+        echo "Cloning $v..."
+        git clone --single-branch --branch "$v" git@github.com:tonysm/turbo-laravel.git "resources/sources/$v"
+        ln -s "resources/sources/$v/docs" "resources/docs/$v"
+    fi;
+done
