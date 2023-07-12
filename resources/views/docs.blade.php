421afc25bf73cac8889dc0533f66c3748d68a184
Tony Messias <tonysm@hey.com>
2023-07-11 23:53:55 -0300

    Reuse the version form partial

--- resources/views/docs.blade.php
+++ resources/views/docs.blade.php
@@ -1,17 +1,8 @@
 @extends('layouts.main')
 
 @section('mobileIndex')
-    <form data-controller="autosubmit" data-action="change->autosubmit#submit" action="{{ route('change-version') }}" method="get" class="w-full pr-2">
-        <input type="hidden" name="page" value="{{ $page }}" />
-        <div class="flex flex-col w-full space-y-2">
-            <label for="version" class="text-sm text-gray-400 uppercase">{{ __('Version') }}</label>
-            <select name="version" id="version" class="text-sm border border-gray-300 rounded">
-                @foreach ($versions as $version)
-                <option value="{{ $version }}" @if ($version === $currentVersion) selected @endif >{{ $version }}</option>
-                @endforeach
-            </select>
-        </div>
-    </form>
+    @include('partials._versions_form')
+
     {!! $index ?? '' !!}
 @stop
 
